74e51e87bc3242e0a1262d563dbfc9929aeb0f14
icld <ian@icldesign.com>
2021-07-23 12:40:33 -0700

    connect and test api

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
+/* eslint-disable no-unused-vars */
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// import config from './config';
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/courses')
+      .then((response) => setData(response.data))
+      .catch((error) => console.log('Error fetching and parsing data', error));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        {data.map((item) => (
+          <h1>{item.title}</h1>
+        ))}
       </header>
     </div>
   );

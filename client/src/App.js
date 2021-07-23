2007289b00f61f8561dc561645c26e76972aa074
icld <ian@icldesign.com>
2021-07-23 13:55:07 -0700

    add Courses Route

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import axios from 'axios';
 
 import Header from './components/Header';
+import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 // import config from './config';
 
@@ -29,6 +30,7 @@ function App() {
       <div className='App'>
         <Header />
         <Switch>
+          <Route exact path='/' component={Courses} />
           <Route path='/courses/:id' component={CourseDetail} />
         </Switch>
       </div>
--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -1,34 +1,39 @@
+import { Link, NavLink } from 'react-router-dom';
+
 const Courses = () => {
   return (
     <main>
-      <div class='wrap main--grid'>
-        <a class='course--module course--link' href='course-detail.html'>
-          <h2 class='course--label'>Course</h2>
-          <h3 class='course--title'>Build a Basic Bookcase</h3>
-        </a>
-        <a class='course--module course--link' href='course-detail.html'>
-          <h2 class='course--label'>Course</h2>
-          <h3 class='course--title'>Learn How to Program</h3>
-        </a>
-        <a class='course--module course--link' href='course-detail.html'>
-          <h2 class='course--label'>Course</h2>
-          <h3 class='course--title'>Learn How to Test Programs</h3>
-        </a>
-        <a class='course--module course--add--module' href='create-course.html'>
-          <span class='course--add--title'>
+      <div className='wrap main--grid'>
+        <Link className='course--module course--link' to='course-detail.html'>
+          <h2 className='course--label'>Course</h2>
+          <h3 className='course--title'>Build a Basic Bookcase</h3>
+        </Link>
+        <Link className='course--module course--link' to='course-detail.html'>
+          <h2 className='course--label'>Course</h2>
+          <h3 className='course--title'>Learn How to Program</h3>
+        </Link>
+        <Link className='course--module course--link' to='course-detail.html'>
+          <h2 className='course--label'>Course</h2>
+          <h3 className='course--title'>Learn How to Test Programs</h3>
+        </Link>
+        <Link
+          className='course--module course--add--module'
+          to='create-course.html'
+        >
+          <span className='course--add--title'>
             <svg
               version='1.1'
               xmlns='http://www.w3.org/2000/svg'
               x='0px'
               y='0px'
               viewBox='0 0 13 13'
-              class='add'
+              className='add'
             >
               <polygon points='7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 '></polygon>
             </svg>
             New Course
           </span>
-        </a>
+        </Link>
       </div>
     </main>
   );

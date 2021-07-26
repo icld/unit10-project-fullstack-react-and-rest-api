77550e00c4a1e58607d3f8d3dabfd2e25090da52
icld <ian@icldesign.com>
2021-07-25 19:22:42 -0700

    add error component and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 /* eslint-disable no-unused-vars */
 import { useState, useEffect, useContext } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-  NavLink,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 import NotFound from './components/NotFound';
+import Error from './components/Error';
 
 function App(props) {
   return (
@@ -21,7 +16,8 @@ function App(props) {
         <Switch>
           <Route exact path='/' component={Courses} />
           <Route path='/courses/:id' component={CourseDetail} />
-          <Route path='/notfound' component={NotFound} />
+          <Route path='/error' component={Error} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     </Router>
---
+++ client/src/components/Error.jsx
@@ -0,0 +1,10 @@
+const Errors = () => (
+  <main>
+    <div class='wrap'>
+      <h2>Error</h2>
+      <p>Sorry! We just encountered an unexpected error.</p>
+    </div>
+  </main>
+);
+
+export default Errors;
